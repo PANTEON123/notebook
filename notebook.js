4f8ea0a46269247804589c4bdde1343b56142441
PANTEON <ebrikov@bk.ru>
2022-07-03 03:05:01 +0500

    created App

---
+++ notebook.js
@@ -0,0 +1,24 @@
+var vm = new Vue({
+    el: '#space',
+    data: {
+        textHeader: 'List of my tasks or to-dos',
+        taskas: [
+            {message:'Покормить кота', mode: false},
+            {message:'Покормить Настю', mode: false},
+            {message:'23', mode: false},
+        ],
+        connection: '',
+        styleObject: {
+            textDecorationLine: 'line-through',
+        },
+    },
+    methods: {
+        addTask() {
+            this.taskas.push({ message: this.connection, mode: false, del:false });
+            this.connection = '';
+        },
+        removeElement: function(index) {
+            this.taskas.splice(index, 1)
+        }
+    }
+});
